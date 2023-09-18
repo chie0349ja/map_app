7357ec2cf59e05c5dd3226b889a82c1ab4aef018
@chie-python <momochie0712@gmail.com>
2023-09-18 20:10:34 +0900

    Add category

--- app/helpers/spots_helper.rb
+++ app/helpers/spots_helper.rb
@@ -1,6 +1,11 @@
 module SpotsHelper
   def marker_content(spot)
     html = ""
+    html << <<-HTML
+    <div class="spot-category #{'background-category1' if spot.category.name == '農園'} #{'background-category2' if spot.category.name == '産直店'} #{'background-category3' if spot.category.name == '園芸店'} #{'background-category4' if spot.category.name == '種'}">
+      #{spot.category.name}
+    </div>
+    HTML
     html << "#{link_to spot.name, spot_path(spot)}<br />"
     if spot.photo.attached?
       html << "#{image_tag(spot.photo, width: '100%')}<br />"
