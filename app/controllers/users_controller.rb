16ef7435b20e1f5d40a43ef8078988aa2d572506
@chie-python <momochie0712@gmail.com>
2023-10-02 22:39:12 +0900

    Edit user-spots

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @spots = user.spots
+    @spots = user.spots.order("created_at DESC")
   end
 end
