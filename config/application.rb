23e241822f5d11de69cf214e2befd01a54fc2786
@chie-python <momochie0712@gmail.com>
2025-02-16 15:30:34 +0900

    20250216_change_ruby_vaersion

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,6 +11,11 @@ module MapApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -19,6 +23,5 @@ module MapApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.active_storage.variant_processor = :mini_magick
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_235838) do
-  create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
+ActiveRecord::Schema[7.2].define(version: 2025_02_16_051014) do
+  create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_235838) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb3", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,13 +33,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_235838) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "sns_credentials", charset: "utf8", force: :cascade do |t|
+  create_table "sns_credentials", charset: "utf8mb3", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
     t.bigint "user_id"
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_235838) do
     t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
-  create_table "spots", charset: "utf8", force: :cascade do |t|
+  create_table "spots", charset: "utf8mb3", force: :cascade do |t|
     t.float "lat"
     t.float "lng"
     t.string "name"
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_235838) do
     t.integer "user_id"
   end
 
-  create_table "users", charset: "utf8", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
